dc92b1a5297113b122a6498c6c35fb2cc4fd89cd
Good Boy <jonathan.digay1@mail.com>
2021-05-15 21:55:54 +0800

    add backend

--- src/components/about.js
+++ src/components/about.js
@@ -12,8 +12,7 @@ export default function about() {
                 <div className="about_sec">
                     <h1>ABOUT</h1>
                     <p>Iam Jonathan Digay From the province of Marinduque 
-                        20 years old trying to master react liblary the 
-                        most popular!, i start learning react since 2021,
+                     i start learning react since 2021,
                     </p>
                     <div className="filter"></div>
                 </div>
