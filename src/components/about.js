93f1c56b58b1d7180074054e604723f6e079b610
Loading <jonathan.digay1@mail.com>
2021-06-01 17:03:29 +0800

    change color

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,7 @@ export default function about() {
                 <div className="about_sec">
                     <h1>ABOUT</h1>
                     <p>Iam Jonathan Digay From the province of Marinduque 
-                     i start learning react since 2021,
+                     i start learning react since february 2021,
                     </p>
                     <div className="filter"></div>
                 </div>
