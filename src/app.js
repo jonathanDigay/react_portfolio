300d234c2484242de45ef44c4359eb180d68896b
Loading <jonathan.digay1@mail.com>
2021-06-06 13:40:45 +0800

    remove about

--- src/app.js
+++ src/app.js
@@ -2,26 +2,25 @@ import React from "react"
 
 
 // style
- import "./components/app.scss"
+import "./components/app.scss"
 // components
 import Navbar from "./components/navbar.js"
 import Hero from "./components/hero.js"
-import About from "./components/about.js"
 import Projects from "./components/projects.js"
 import Contact from "./components/contact.js"
 import Footer from "./components/footer.js"
 
-const App=()=>{
-     
+const App = () => {
+
     return (
         <>
-           <Navbar/>
-           <Hero/>
-           <Projects/>
-           <About/>
-           <Contact/>
-           <Footer/>
+            <Navbar />
+            <Hero />
+            <Projects />
+            <About />
+            <Contact />
+            <Footer />
         </>
-    )           
+    )
 }
 export default App
